78413200d83828b0b3c189f010f0994f8945cab4
Bob Timm <bajabob.tx@gmail.com>
2015-03-31 12:45:47 -0500

    base gui created for displaying each GA algorithm

---
+++ src/CircuitGARunnable.java
@@ -0,0 +1,133 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+/**
+ * Simple runnable class for creating algorithm searching threads and managing
+ *  the display of its results in a GUI pane
+ */
+public class CircuitGARunnable extends JPanel implements Runnable
+{
+	
+	public final static int MAX_HEIGHT = 250;
+	public final static int MAX_WIDTH = 800;
+	
+	/**
+	 * the solution we are searching for
+	 */
+	private TruthTable tableToFind;
+	
+	/**
+	 * the algorithm searching for circuits
+	 */
+	private CircuitGA searchAlgorithm;
+	
+	/**
+	 * collection of labels that are updated in realtime
+	 */
+	private JLabel foundSolutions;
+	
+	/**
+	 * handles drawing the gates to screen
+	 */
+	private GatesDisplay gatesDisplay;
+	
+	/**
+	 * Start stop button
+	 */
+	protected JButton startStopButton;
+	
+	
+	public CircuitGARunnable( TruthTable tableToFind)
+	{
+		super();
+		
+		this.tableToFind = tableToFind;
+		searchAlgorithm = new CircuitGA(tableToFind);
+		
+		/**
+		 * Create left control panel
+		 */
+		JPanel controlPanel = new JPanel();
+		controlPanel.setLayout( new BoxLayout(controlPanel, BoxLayout.Y_AXIS) );
+		controlPanel.setPreferredSize(new Dimension(200, MAX_HEIGHT));
+		
+		// add the name of the circuit we are searching for
+		Font font = this.getFont();
+		Font boldFont = new Font(font.getFontName(), Font.BOLD, font.getSize()+2);
+		final JLabel name = new JLabel(tableToFind.getName());
+		name.setForeground( Color.RED );
+		name.setFont( boldFont );
+		controlPanel.add( name );
+		
+		foundSolutions = new JLabel();
+		controlPanel.add(foundSolutions);
+		
+		// add start/stop button
+		startStopButton = new JButton("Start");
+		startStopButton.addActionListener( new ActionListener()
+		{
+			public void actionPerformed( ActionEvent e )
+			{
+				
+			}
+		});
+		startStopButton.setMaximumSize(new Dimension(200, 40));
+		controlPanel.add( startStopButton );
+		controlPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		
+		/**
+		 * Create right gate GUI
+		 */
+		gatesDisplay = new GatesDisplay();
+		gatesDisplay.setBounds(0, 0, 600, MAX_HEIGHT);
+		
+		/**
+		 * Add to this panel
+		 */
+		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		this.add(controlPanel);
+		this.add(gatesDisplay);
+		this.setPreferredSize(new Dimension(MAX_WIDTH, MAX_HEIGHT));
+
+	}
+	
+	
+	/**
+	 * thread entry
+	 */
+	public void run()
+	{
+    }
+	
+	/**
+	 * Called when the GUI wants the stats updated
+	 */
+	public void onUpdateGUI()
+	{
+		foundSolutions.setText( "Found Solutions: 1");
+		gatesDisplay.repaint();
+	}
+	
+	private static class GatesDisplay extends JPanel
+	{
+		public void paintComponent( Graphics g )
+		{
+			super.paintComponent( g );
+			g.setColor( Color.BLACK );
+			g.fillRect( 0, 0, 600, MAX_HEIGHT );
+		}
+	}
+	
+}
--- src/MainGA.java
+++ src/MainGA.java
@@ -1,9 +1,66 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
 
 public class MainGA {
 
+	/**
+	 * Collection of circuit finding threads
+	 */
+	static ArrayList<CircuitGARunnable> runnables;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		runnables = new ArrayList<CircuitGARunnable>();
+		
+		// Carry-Out Circuit
+		boolean[] co = {false, false, false, true, false, true, true, true};
+		TruthTable carryOutTable = new TruthTable("Carry-Out", 3, co);
+		runnables.add(new CircuitGARunnable(carryOutTable));
+		
+		/**
+		 * Create GUI
+		 */
+		JPanel panes = new JPanel();
+		panes.setLayout( new BoxLayout(panes, BoxLayout.Y_AXIS) );
+		
+		// add sub-panes to this window
+		for( CircuitGARunnable runnable :  runnables)
+		{
+			panes.add( runnable );
+		}
+		
+		// start processing
+		for( CircuitGARunnable runnable :  runnables)
+		{
+			(new Thread(runnable)).start();
+		}
+		
+		JFrame window = new JFrame( "Team #7 - Project 3" );
+		window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		window.setSize( CircuitGARunnable.MAX_WIDTH, runnables.size()*CircuitGARunnable.MAX_HEIGHT );
+		window.setContentPane( panes );
+		window.setVisible( true );
+		window.setResizable( false );
+		
+		// update GUI
+		ActionListener updateGUI = new ActionListener()
+		{
+			public void actionPerformed( ActionEvent evt )
+			{
+				for( CircuitGARunnable runnable :  runnables)
+				{
+					runnable.onUpdateGUI();
+				}
+			}
+		};
+		new Timer( 250, updateGUI ).start();
 	}
 
 }
